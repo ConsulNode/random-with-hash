d0cc7471e62f9f8ff2cce4596825c2b1e45541cc
ConsulNode <52566570+ConsulNode@users.noreply.github.com>
2019-09-18 22:16:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,83 +1,151 @@
-import 'dotenv/config'
-import axios from 'axios'
-import cron from 'node-cron'
+import 'dotenv/config';
+import https from 'https';
+import axios from 'axios';
+import cron from 'node-cron';
 
 
-const API_VALIDATOR = 'https://explorer-api.apps.minter.network/api/v1/validators/Mpc9fc1052e075054cdbfb6443a6d14d97be9d4f19a10505c4323b52a78ca4bb18'
-const API_COINS = 'https://explorer-api.apps.minter.network/api/v1/coins'
+/**
+ * API variables
+ * 
+ * It is possible to verify list of delegators
+ * for the block when winner was chosen.
+ * Add height of the block to the CONSULNODE_API link.
+ */
+const EXPLORER_API = 'https://explorer-api.apps.minter.network/api/v1/';
+const CONSULNODE_API = 'https://overview.consulnode.com/candidate?pub_key=Mpc9fc1052e075054cdbfb6443a6d14d97be9d4f19a10505c4323b52a78ca4bb18&height=';
 
 
+// Start process every day
 cron.schedule(process.env.CRON, () => {
-  chooseWinner()
-})
+  chooseWinner();
+});
 
 
-let coins = []
-let delegators = []
-let winner = ''
+let latestBlock = {};
+let coins = [];
+let validator = {};
 
 
 async function chooseWinner() {
-  await getCoins()
-  await getDelegators()
-  
-  winner = ''
+  latestBlock = {};
+  coins = [];
+  validator = {};
 
-  const winnerIndex = Math.floor(Math.random() * delegators.length)
-  winner = delegators[winnerIndex].address
+  latestBlock = await getLatestBlock();
+  coins = await getCoins();
+  validator = await getDelegators(latestBlock.height, coins);
 
-  rewardForWinner = 500
-  if (new Date().getDay() === 5) rewardForWinner = 1000
-  if (new Date().getDate() === 27) rewardForWinner = 5000
-  
-  axios.get(encodeURI(`https://api.telegram.org/bot${process.env.BOT}/sendMessage?chat_id=${process.env.CHAT}&text=${new Date().toISOString().split('T')[0]}\r\n\r\n🎁 Подарочные ${rewardForWinner} BIP будут зачислены\r\nⓂ️ ${winner}`))
-}
+  if (validator.delegators.length > 0) {
+    const hashNumber = parseInt(latestBlock.hash.substring(2, 6), 16);
+    const winnerIndex = hashNumber % validator.delegators.length;
+    const winner = validator.delegators[winnerIndex];
 
+    let rewardForWinner = 500;
+    if (new Date().getDay() === 5) rewardForWinner = 1000;
+    if (new Date().getDate() === 27) rewardForWinner = 5000;
+
+    // Send message via Telegram Bot
+    sendFeed(`${new Date().toISOString().split('T')[0]}\r\nБлок: ${latestBlock.height}\r\n\r\n🎁 Подарочные ${rewardForWinner} BIP будут зачислены\r\nⓂ️ ${winner.address}`);
+  }
+};
+
+
+function getLatestBlock() {
+  return new Promise(resolve => {
+    let latestBlock = {};
+
+    axios.get(`${EXPLORER_API}blocks`)
+      .then(response => {
+        const block = response.data.data[0];
+
+        latestBlock = {
+          height: block.height,
+          hash: block.hash,
+        };
+
+        resolve(latestBlock);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  });
+};
 
 function getCoins() {
   return new Promise(resolve => {
-    coins = []
+    let coins = [];
 
-    axios.get(API_COINS)
+    axios.get(`${process.env.EXPLORER_API}coins`)
       .then(response => {
-        coins = response.data.data
-        resolve()
+        coins = response.data.data;
+
+        resolve(coins);
       })
       .catch(error => {
-        console.log(error)
-      })
-  })
-}
+        console.log(error);
+      });
+  });
+};
 
-function getDelegators() {
+function getDelegators(latestBlockHeight, coins) {
   return new Promise(resolve => {
-    delegators = []
+    let delegators = [];
 
-    axios.get(API_VALIDATOR)
+    const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+    axios.get(`${CONSULNODE_API}${latestBlockHeight}`, { httpsAgent })
       .then(response => {
-        response.data.delegator_list.forEach(item => {
-          if (verifyCoin(item.coin)) {
-            const index = delegators.findIndex(i => i.address === item.address)
+        // Change stakes to float numbers
+        const stakes = response.data.result.stakes.map(stake => {
+          stake.value = +(stake.value * Math.pow(10, -18)).toFixed(4);
+          stake.bip_value = +(stake.bip_value * Math.pow(10, -18)).toFixed(4);
 
-            if (index === -1)
-              delegators.push({ address: item.address, value: Number(Number(item.bip_value).toFixed(2)) })
-            else
-              delegators[index].value = Number((delegators[index].value + Number(Number(item.bip_value).toFixed(2))).toFixed(2))
+          return stake;
+        });
+
+        // Combine different stakes from the same wallet
+        stakes.forEach(item => {
+          if (verifyCoin(item.coin, coins)) {
+            const index = delegators.findIndex(i => i.address === item.owner);
+
+            if (index === -1) {
+              let delegator = {
+                address: item.owner,
+                total_value: item.bip_value,
+              };
+
+              delegators.push(delegator);
+            } else
+              delegators[index].total_value = +(delegators[index].total_value + item.bip_value).toFixed(4);
           }
-        })
+        });
 
-        delegators = delegators.filter(item => item.value >= 10)
-        resolve()
+        // DESC sort stakes by total value and wallet address
+        delegators.sort((a, b) => {
+          if (a.total_value < b.total_value)
+            return 1;
+          if (a.total_value > b.total_value)
+            return -1;
+          
+          if (a.address < b.address)
+            return 1;
+          if (a.address > b.address)
+            return -1;
+          
+          return 0;
+        });
+
+        resolve({
+          delegators,
+        });
       })
       .catch(error => {
-        console.log(error)
-      })
-  })
-}
+        console.log(error);
+      });
+  });
+};
 
-
-function verifyCoin(coin) {
-  if (coin === 'BIP') return true
-  if (coins.find(item => item.symbol === coin).crr >= 55) return true
-  return false
-}
+function verifyCoin(coin, coins) {
+  if (coin === 'BIP') return true;
+  if (coins.find(item => item.symbol === coin).crr >= 50) return true;
+  return false;
+};
